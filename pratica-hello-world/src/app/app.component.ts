b3389d7996e7157f01a964a77d392a8e34c49a5c
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2023-09-24 22:15:04 -0300

    Add files via upload

---
+++ pratica-hello-world/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hello-world-atvd';
+  aluno = 'Márcio José da Silva';
+}
